a5af1d584c8b388748007c611dd2d1d81d664b53
thilo <thilokloos@gmx.de>
2022-09-09 16:47:53 +0200

    Added SFML to program files and makefile so its not required to add it manually anymore.

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Asteroids")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Boids")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
